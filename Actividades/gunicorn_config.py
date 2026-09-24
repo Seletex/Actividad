ec397feb2cf64c9bbae3637271f6a67ef5a56dcf
Steven Ruiz <sarr1998@hotmail.com>
2026-09-24 17:53:36 -0500

    Respeta la concurrencia definida por Render

--- Actividades/gunicorn_config.py
+++ Actividades/gunicorn_config.py
@@ -4,7 +4,7 @@ import os
 
 port = os.environ.get("PORT", "8000")
 bind = f"0.0.0.0:{port}"
-workers = 2  # Adjust based on available resources
+workers = int(os.environ.get("WEB_CONCURRENCY", "1"))
 threads = 4
 timeout = 120
 worker_class = "gthread"
